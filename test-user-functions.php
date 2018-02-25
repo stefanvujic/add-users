3b4dcef5f56d1838780a8cc9695b599078b2c6e1
Unknown <stefanvujic576@gmail.com>
2018-02-25 15:42:52 +0000

    random male,female first name generation

--- test-user-functions.php
+++ test-user-functions.php
@@ -11,10 +11,13 @@ License: GPLv2 or later
 Navigation:
 1. install/uninstall, activate, deactivate plugin
 2. Admin interface
+3. Generate User
+	- User Name
+	- User Email
+	- User Password
 */
 
 // --- install/uninstall, activate, deactivate plugin ---- //
-
 function test_user_activation() {
 global $wpdb;
 
@@ -68,7 +71,6 @@ register_deactivation_hook(__FILE__, 'test_user_remove_database');
 
 
 // --- Admin interface ---- //
-
 function test_user_menu_item() {
 	add_options_page('WP Test User', 'WP Test User', 'manage_options', 'wp-test-user_slug', 'test_user_interface');
 }
@@ -110,6 +112,42 @@ function test_user_interface() {
 	elseif ($_POST['gender'] == 'female') {
 		update_option('male_or_female', 'female');
 	}
-	update_option('user_number', 'female');
+	update_option('user_number', $_POST['number_of_users']);
 }
+
+
+// --- Generate User ---- //
+function generate_user() {
+global $wpdb;
+
+	//User Name
+	if (isset($_POST['generate_info']) && $_POST['gender'] == 'male') {
+
+		$get_male_names = $wpdb->get_results('SELECT user_name FROM test_user_names WHERE user_gender = "male" AND name_type = "firstname"');
+
+		$male_names_array = array();
+		foreach ($get_male_names as $male_name_key1 => $male_name1) {
+			foreach ($male_name1 as $male_name_key2 => $male_name) {
+				array_push($male_names_array, $male_name);
+			}
+		}
+		$key = array_rand($male_names_array, 1);
+		$random_male_name = $male_names_array[$key];
+	}
+
+	elseif (isset($_POST['generate_info']) && $_POST['gender'] == 'female') {
+
+		$get_female_names = $wpdb->get_results('SELECT user_name FROM test_user_names WHERE user_gender = "female" AND name_type = "firstname"');
+
+		$female_names_array = array();
+		foreach ($get_female_names as $female_name_key1 => $female_name1) {
+			foreach ($female_name1 as $female_name_key2 => $female_name) {
+				array_push($female_names_array, $female_name);
+			}
+		}
+		$key = array_rand($female_names_array, 1);
+		$random_female_name = $female_names_array[$key];		
+	}
+}
+generate_user();
 ?>
